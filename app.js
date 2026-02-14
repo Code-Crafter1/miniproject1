d3c91ee178fcddea61a6c93eaeb4ec6d2df80b04
Code-Crafter1 <sakshikumarizen@gmail.com>
2026-02-14 23:02:45 +0530

    login setup

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const userModel = require("./models/user");
+const postModel = require("./models/post");
 const cookieParser = require("cookie-parser");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 app.set("view engine", "ejs");
 
@@ -14,10 +16,15 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get("/login", (req, res) => {
+  //   console.log("hey");
+  res.render("login");
+});
+
 app.post("/register", async (req, res) => {
   let { name, age, email, password, username } = req.body;
   let user = await userModel.findOne({ email });
-  if (user) return res.status(500).send("user already exists");
+  if (user) return res.status(500).send("user already registered");
 
   bcrypt.genSalt(10, (err, salt) => {
     bcrypt.hash(password, salt, async (err, hash) => {
@@ -28,7 +35,18 @@ app.post("/register", async (req, res) => {
         password: hash,
         username,
       });
+      let token = jwt.sign({ email: email, userid: user._id }, "shhhh");
+      res.cookie("token", token);
+      res.send("user created");
     });
   });
+});
+
+app.post("/login", async (req, res) => {
+  let { email, password } = req.body;
+  let user = await userModel.findOne({ email });
+  if (!user) return res.status(500).send("something went wrong");
+
+ 
 });
 app.listen(3000);
